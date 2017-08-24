4b05e8bf0a5a8c8d127b84b5c24828a54443d230
Thomas Waldecker <thomas.waldecker@tq-group.com>
2017-08-24 13:33:35 +0200

    Initial commit.

---
+++ assemblyinfo.js
@@ -0,0 +1,44 @@
+﻿const fs = require('fs');
+
+var path = 'test/AssemblyInfo.cs';
+
+var keywords = {
+    "AssemblyTitle": null,
+    "AssemblyDescription": null,
+    "AssemblyConfiguration": null,
+    "AssemblyCompany": null,
+    "AssemblyProduct": null,
+    "AssemblyCopyright": null,
+    "AssemblyTrademark": null,
+    "AssemblyCulture": null,
+    "ComVisible": null,
+    "Guid": null,
+    "AssemblyVersion": null,
+    "AssemblyFileVersion": null,
+    "AssemblyInformationalVersion": null,
+    "InternalsVisibleTo": null,
+}
+
+function read(path, callback) {
+
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+        }
+
+        Object.keys(keywords).forEach((value) => {
+            var res = data.match(new RegExp(value+"\\(\"([^\"]+?)\"\\)"));
+            if(res && res[1]){
+                keywords[value] = res[1];
+            }
+        });
+
+        callback(keywords);
+    });
+}
+
+var assemblyinfo = {
+    read: read,
+}
+
+module.exports = assemblyinfo;
---
+++ test/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Title")]
+[assembly: AssemblyDescription("Description")]
+[assembly: AssemblyConfiguration("Configuration")]
+[assembly: AssemblyCompany("Company")]
+// A comment
+[assembly: AssemblyProduct("Product")]
+[assembly: AssemblyCopyright("Copyright")]
+[assembly: AssemblyTrademark("Trademark")]
+[assembly: AssemblyCulture("Culture")]
+[assembly: ComVisible(false)]
+[assembly: Guid("12e0df89-65cf-4de1-877d-79a9d4f87daa")]
+[assembly: AssemblyVersion("1.1.0")]
+// A Comment in the file.
+[assembly: AssemblyFileVersion("1.3.4")]
+[assembly: AssemblyInformationalVersion("2.3.4.3")]
+[assembly: InternalsVisibleTo("Internals")]
